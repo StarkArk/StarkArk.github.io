47e2586e0b49e987e47b3ef95cd89e9a48fb003a
StarkArk <91919606+StarkArk@users.noreply.github.com>
2024-01-05 15:46:57 -0800

    Test Page2

--- js/test_bar.js
+++ js/test_bar.js
@@ -60,8 +60,6 @@ function stackedBar(year) {
             // hovermode: 'closest',
             // template: 'plotly_white',
             barmode: 'stack', // Stacked bar chart
-            // width: 250 // Adjust the Height of the entire chart 
-            height: 400 // Adjust the Height of the entire chart 
           };
 
           // Render the chart
